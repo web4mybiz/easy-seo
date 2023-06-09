5849590cd942f9388d61d830ed4bd3aedf1d7af1
Rizwan Iliyas <web4mybiz@gmail.com>
2023-06-09 23:26:14 +0530

    Setting up admin page

--- inc/classes/admin/class-admin-options.php
+++ inc/classes/admin/class-admin-options.php
@@ -2,4 +2,16 @@
 
 class EasySEOAdminOptions{
 	
+	function __construct(){
+		add_action( 'admin_menu', array( $this, 'easy_seo_admin_menu') );
+		add_action( 'admin_init', array( $this, 'easy_seo_admin_page') );
+	}
+
+	function easy_seo_admin_menu(){
+		add_options_page( 'Easy SEO', 'Easy SEO', 'manage_options', 'easy-seo', 'easy_seo_admin_page' );
+	}
+
+	function easy_seo_admin_page(){
+		// displays the menu item for Easy SEO plugin admin page settings
+	}
 }
