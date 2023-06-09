48b5fea79257d74259e5b9465e55c80594ad7632
Rizwan Iliyas <web4mybiz@gmail.com>
2023-06-09 19:45:21 +0530

    Plugin header updated

---
+++ easy-seo.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Easy SEO
+ * Plugin URI:        https://github.com/web4mybiz/easy-seo
+ * Description:       Simple plugin to improve review and improve SEO.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rizwan Iliyas
+ * Author URI:        https://wordpress.org/support/users/irizweb/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://wp-media.me/easy-seo/
+ * Text Domain:       wpmedia-easy-seo
+ * Domain Path:       /languages
+ */
